3241a25b644772f6393dc90989437ca672032300
Krzysztof Saniewski <krzysztof.saniewski@protonmail.com>
2023-08-23 20:04:58 +0200

    add new pages and update navigation

---
+++ main.js
@@ -0,0 +1,11 @@
+//alert('alert z pliku main.js');
+
+/*alert('alert z pliku main.js');
+console.log('console log test');*/
+
+const name = "Tadeusz";
+const age = "57";
+
+console.log('console log test');
+
+console.log(`nazywam się ${name} i mam ${age} lat`)
