ee2c9e8fb0d20f10041c34bc9fd06fee30eec46d
Krzysztof Saniewski <krzysztof.saniewski@protonmail.com>
2023-08-27 16:46:15 +0200

    BEM & DOM

--- javascript/main.js
+++ javascript/main.js
@@ -9,3 +9,15 @@ const age = "57";
 console.log('Cześć!');
 
 console.log(`nazywam się ${name} i mam ${age} lat`)
+
+const header = document.querySelector('.page-header__heading--js');
+console.log('header');
+
+header.innerHTML = `nazywam się ${name} i mam ${age} lat`;
+
+
+console.log(header.style); //wyświetla style (listę wszystkich właściwości) danego elementu w konsoli;
+
+header.style.color='red'; //modyfikuje kolor header-a w DOM;
+
+console.log(header.style.color); //wyświetla kolor danego elementu
