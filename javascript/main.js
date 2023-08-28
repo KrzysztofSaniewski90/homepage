d229667e254ec4cda80f5144ce6a45dfb166cc4b
Krzysztof Saniewski <krzysztof.saniewski@protonmail.com>
2023-08-29 00:55:28 +0200

    JS Functions and Objects

--- javascript/main.js
+++ javascript/main.js
@@ -24,8 +24,61 @@ console.log(header.style.color); //wyświetla kolor danego elementu
 
 
 
-//praca domowa IV
+//PRACA DOMOWA IV
 
 const injectContent = document.querySelector('.article__paragraph--js');
 console.log(injectContent);
 injectContent.innerHTML = 'JE-JE-JE-JESTEM WSZCZYKNIENTY DŻEJESEM';
+
+
+
+//FUNKCJE
+
+/*function calculate(myNumber) {
+    myNumber = myNumber+3;
+    console.log(myNumber);
+    return myNumber;
+}
+
+calculate(1);
+const result = calculate(1);
+console.log(result);*/
+
+//funkcja strzałkowa "Fat arrow" =>
+
+const calculateFat = (myFatNumber) => {
+    myFatNumber = myFatNumber+8;
+    console.log(myFatNumber);
+    return myFatNumber;
+}
+
+calculateFat(1);
+const resultFat = calculateFat(1);
+console.log(resultFat); // tę funkcję można skrócić jeszcze bardziej - poniżej
+
+const fatCalculate = myFatNumber => (myFatNumber+8)
+console.log(fatCalculate(9));
+
+
+
+//OBIEKTY
+ const deathStar = {
+    diameter : 1200,
+    fire : (target) => {
+        console.log(`${target} destroyed 🔥`)
+    },
+    isOperating: true,
+    levels: 357,
+    name: 'Death Star',
+    population: 2354,
+    commander: {
+        name: 'Darth Vader'
+    },
+ }
+
+ console.log(deathStar.name)
+ deathStar.fire('Alderaan')
+ deathStar.fire('Scarif')
+
+ console.log(`Gwiazda Śmierci ma średnicę ${deathStar['diameter']} metrów.`)
+ console.log(`pojazd zarejestrowany jest na ${deathStar.commander.name}`)
